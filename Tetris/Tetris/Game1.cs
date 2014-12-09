acc876b543a54849a7e67cbc7269a6ef0a17d87a
YamilYanez <yamilenriqueyanezmorales@hotmail.com>
2014-12-08 18:10:51 -0600

    Tablero

--- Tetris/Tetris/Game1.cs
+++ Tetris/Tetris/Game1.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Tetris.Piezas;
+using Tetris.Table;
 
 namespace Tetris
 {
@@ -22,9 +23,12 @@ namespace Tetris
         //Creas una figura, El primer parametro define el color(1-5) y el segundo parametro define forma de la figura(1-7)
         Pieza p = new Pieza(5, 1);
         //Puedes rotar la figura presionando D
+        Tablero t = new Tablero();
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 384;
+            graphics.PreferredBackBufferHeight = 704;
             Content.RootDirectory = "Content";
         }
 
@@ -37,6 +41,7 @@ namespace Tetris
         protected override void LoadContent() {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             p.LoadContent(Content);
+            t.LoadContent(Content);
         }
 
         //Descargar contenido
@@ -56,6 +61,7 @@ namespace Tetris
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.Gray);
             spriteBatch.Begin();
+            t.Draw(spriteBatch);
             p.Draw(spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
---
+++ Tetris/Tetris/Table/Tablero.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tetris.Table
+{
+    public class Tablero
+    {
+        Texture2D fondo, marco;
+        char[,] tablero = new char[22, 12] { 
+        #region Inicializacion del tablero
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'U'},
+        { 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U'},
+        #endregion
+        };
+
+        public Tablero() {
+        }
+
+        public void LoadContent(ContentManager Content){
+            fondo = Content.Load<Texture2D>("Tablero/BG");
+            marco = Content.Load<Texture2D>("Tablero/Marco");
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < 22; i++) {
+                for (int j = 0; j < 12; j++) {
+                    if (tablero[i, j] == 'X') {
+                        spriteBatch.Draw(fondo, new Vector2(32 * j, 32 * i), Color.White);
+                    }
+                    if (tablero[i, j] == 'U') {
+                        spriteBatch.Draw(marco, new Vector2(32 * j, 32 * i), Color.White);
+                    }
+                }
+            }
+        }
+
+    }
+}
